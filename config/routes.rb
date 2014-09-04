f9286dffde029cf01e573c8634571ab8607a5b0b
Dan Dimond <dtdimond@gmail.com>
2014-09-04 20:27:31 +1000

    Add password digest to users, bcrypt-ruby to gemfile, has_secure_password to users model. Also added new user form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+  get 'register', to: 'users#new'
+
   resources :posts, except: :destroy do
     resources :comments, only: :create
   end
