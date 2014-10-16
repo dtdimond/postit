a689b963502a43cb9f875c004761ac251721909b
Dan Dimond <dtdimond@gmail.com>
2014-10-16 17:45:07 +1100

    Fix vote validations scope.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,5 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :voteable, polymorphic: true
   belongs_to :user
 
-  validates_uniqueness_of :user, scope: :voteable
+  validates_uniqueness_of :user, scope: [:voteable_id, :voteable_type]
 end
